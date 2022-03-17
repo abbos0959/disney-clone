b14b7b107e6b2e16a237d1290d22db920c5cb69b
abbos0959 <abbosakromovich777@gmail.com>
2022-03-18 01:35:12 +0500

    diz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Header } from "./component/Header";
 import { Login } from "./component/Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
